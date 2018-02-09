a12d2767bc3ec6f85369317a38ccd56e0a4a80fd
solemsoft.com <solemsoft.com@gmail.com>
2018-02-09 17:36:21 -0500

    actualizacion de librerias y busqueda de vendedor

--- app/src/main/java/com/solemsoft/uberapp/ClienteLoginActivity.java
+++ app/src/main/java/com/solemsoft/uberapp/ClienteLoginActivity.java
@@ -35,7 +35,7 @@ public class ClienteLoginActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if (user != null) {
-                    Intent intent = new Intent(ClienteLoginActivity.this, MapActivity.class);
+                    Intent intent = new Intent(ClienteLoginActivity.this, ClienteMapActivity.class);
                     startActivity(intent);
                     finish();
                     return;
